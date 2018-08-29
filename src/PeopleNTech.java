1c5ef11d1acea61072ac021e2e381a045c677ef3
almrashid <alrashidmamunur@gmail.com>
2018-08-29 14:38:53 -0400

    static variable and Method

--- src/PeopleNTech.java
+++ src/PeopleNTech.java
@@ -1,3 +1,22 @@
 public class PeopleNTech {
     //The use of static variable and static method.
+    static String name;
+    static int id;
+    static String batch;
+    static  String school="PeopleNTech";
+
+    PeopleNTech(){
+    }
+   PeopleNTech(String name, int id , String batch){
+        PeopleNTech.name=name;
+        PeopleNTech.id=id;
+        PeopleNTech.batch=batch;
+
+    }
+
+    public static String    attendAt(String name){
+      PeopleNTech.school=school;   return school;
+  }
+
+
 }
---
+++ src/TestPeopleNTech.java
@@ -0,0 +1,10 @@
+public class TestPeopleNTech extends PeopleNTech {
+    public static void main(String[] args) {
+
+        PeopleNTech piit=new PeopleNTech("AL Rashid",3704,"JavaCourse");
+        System.out.println(name+"---"+ id + "---"+ batch);
+        PeopleNTech.attendAt("Peoplentech");
+
+
+    }
+}
